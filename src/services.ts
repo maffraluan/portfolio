5493a6a2c1c25960b45b2bdb3188bde9d3dd293f
Luan Maffra <luanmaffra@Luans-MacBook-Pro.local>
2020-10-27 15:07:52 -0300

    Added prettier

--- src/services.ts
+++ src/services.ts
@@ -1,16 +1,16 @@
-const apiUrl = `https://api.github.com/users/maffraluan/repos`
+const apiUrl = `https://api.github.com/users/maffraluan/repos`;
 
 interface IFetchProps {
-    id?: number,
-    name: string,
-    description: string,
+  id?: number;
+  name: string;
+  description: string;
 }
 
 const fetchAll = async (): Promise<IFetchProps[]> => {
-    const res = await fetch(apiUrl);
-    const json = await res.json();
+  const res = await fetch(apiUrl);
+  const json = await res.json();
 
-    return json as IFetchProps[]
+  return json as IFetchProps[];
 };
 
-export { fetchAll }
+export { fetchAll };
