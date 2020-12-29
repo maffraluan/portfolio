352d6ee42dd5663a7483c8f730b791402c9789e6
Luan Maffra <luanmaffra@Luans-MacBook-Pro.local>
2020-12-28 23:08:06 -0300

    NotFound Component created

--- src/Pages/Techs/Techs.tsx
+++ src/Pages/Techs/Techs.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Layout from '../../components/Layout/Layout'
+import NotFound from '../../components/NotFound/NotFound'
 
 const Techs: React.FC = () => {
     return (
         <Layout>
+            <NotFound />
         </Layout>
     )
 };
---
+++ src/components/NotFound/NotFound.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles.css';
+
+const NotFound: React.FC = () => {
+  return (
+    <>
+      <section className={'section-not-found'}>
+        <h1>This sections is on development mode</h1>
+      </section>
+    </>
+  );
+};
+
+export default NotFound;
